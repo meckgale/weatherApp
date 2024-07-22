53e844a819300409fe3d33a8ccb86c390c675c4f
meck <meckgalen@gmail.com>
2024-07-22 12:56:56 +0300

    Include live demo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weatherApp
+
 Creating a weather application applciation by using HTML, CCS and javaScript
+
+[Live Demo](https://meckgale.github.io/weatherApp/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-      bundle: path.resolve(__dirname, 'src/index.js')
+    bundle: path.resolve(__dirname, "src/index.js"),
   },
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
     hot: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'todo List',
-        filename: 'index.html',
-        template: 'src/template.html',
-    })
+      title: "Weather App",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
   ],
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
     minimize: false,
   },
 };
